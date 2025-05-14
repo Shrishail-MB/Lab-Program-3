2a2e04e5765f73233ed9a87499971b70dc02b971
Srusti00007 <srustinaik007@gmail.com>
2025-05-14 10:46:18 +0530

    30 Min  Build

---
+++ pgm3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; 
+public class pgm3 { 
+public static void main(String[] args) { 
+Scanner sc = new Scanner(System.in); 
+System.out.print("Enter number of terms: "); 
+int n = sc.nextInt(); 
+int firstTerm = 0, secondTerm = 1; 
+System.out.print("Fibonacci Series till " + n + " terms: "); 
+for (int i = 1; i <= n; ++i) { 
+System.out.print(firstTerm + (i < n ? ", " : "\n")); 
+int nextTerm = firstTerm + secondTerm; 
+
+ firstTerm = secondTerm; 
+secondTerm = nextTerm; 
+} 
+sc.close(); 
+} 
+} 
